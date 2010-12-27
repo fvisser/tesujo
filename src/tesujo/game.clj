cfb25ad825f8d13ed111372511179134812bfdf0
Frank Visser <frank.visser@gmail.com>
2010-12-28 00:34:04 +0100

    simple game definition

--- src/tesujo/game.clj
+++ src/tesujo/game.clj
@@ -1,4 +1,15 @@
 (ns tesujo.game)
 
+(defn make-board [size] (vec (for [_ (range size)] (vec (repeat size 0)))))
 
-(defn make-board [size] (vec (for [_ (range size)] (vec ))))
+(defn is-occupied?
+  "Checks if position of board is filled"
+  [board x y] (if (= (get-in board [x y]) 0)
+		false
+		true))
+
+(defn make-move [board color x y]
+  "First the most simple thing, play if it is unoccupied"
+  (if (is-occupied? board)
+    (assoc-in board [x y] color)
+    board))
